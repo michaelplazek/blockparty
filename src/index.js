17fa847afdbc458d8605dc63fd0dd9f4ff9420ea
Michael Plazek <michael.plazek91@gmail.com>
2018-09-29 10:09:40 -0600

    Added react router and routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory, browserHistory } from 'react-router';
 import { applyMiddleware, createStore } from 'redux'
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducers from './js/reducers'
+import App from './js/App';
 
 const store = createStore(reducers, composeWithDevTools(
     applyMiddleware(thunk),
 ));
 let content = document.getElementById('content');
 ReactDOM.render(
-    <Provider store={store}><div>Index</div></Provider>
+    <Provider store={store}><App /></Provider>
 , content);
---
+++ src/js/screens/Market.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Market = () => (
+    <div>
+        <p>I am the Market</p>
+    </div>
+);
+
+export default Market;
