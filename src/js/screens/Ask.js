e3553936ec2de463a310c433577e94473e84dd36
Michael Plazek <michael.plazek91@gmail.com>
2018-10-24 20:46:56 -0600

    Add slider and name

--- src/js/screens/Ask.js
+++ src/js/screens/Ask.js
@@ -13,12 +13,22 @@ import InputLabel from "@material-ui/core/InputLabel/InputLabel";
 import Select from "@material-ui/core/Select/Select";
 import coins from "../constants/coins";
 import TextField from "@material-ui/core/TextField/TextField";
+import Slider from '@material-ui/lab/Slider';
+
 import withStyles from "@material-ui/core/styles/withStyles";
 
 const styles = () => ({
+	root: {
+		textAlign: 'center',
+		marginTop: '60px'
+	},
 	body: {
 		marginTop: '10px'
-	}
+	},
+	slider: {
+		alignContent: 'center',
+		margin: '20px 60px 20px 60px'
+	},
 });
 
 const Ask = ({
@@ -35,9 +45,8 @@ const Ask = ({
 				>
 					Go Back
 				</Button>
-				<Grid
-					container
-					justify='center'
+				<div
+					className={classes.root}
 				>
 					<Grid
 						item
@@ -45,7 +54,20 @@ const Ask = ({
 					>
 						<Typography variant='display2'>{ask.volume} {ask.coin}</Typography>
 					</Grid>
-				</Grid>
+					<div className={classes.slider}>
+						<Slider
+							value={10}
+							aria-labelledby="label"
+							vertical={false}
+							min={0}
+							max={100}
+						/>
+					</div>
+				</div>
+
+				<div style={{ position: 'absolute', bottom: `${windowHeight/2}px`, left: '10px', zIndex: 100 }}>
+					<Typography variant='display1'>Fort Collins</Typography>
+				</div>
 				<GoogleMapsWrapper
 					markers={[{ id: ask._id, lat: ask.lat, lng: ask.lng }]}
 					height={windowHeight/2 - footerHeight}
