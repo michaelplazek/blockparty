9a2b53ca865a8d1597e60ae9e55fab917821e72a
Michael Plazek <michael.plazek91@gmail.com>
2018-11-11 13:45:11 -0700

    Updated constants for offer feature

---
+++ src/js/constants/offer.js
@@ -0,0 +1,16 @@
+export const DEFAULT_OFFER = {
+  volume: 0,
+  userId: '',
+  owner: '',
+  forBid: false,
+  timestamp: {
+    $date: ""
+  },
+  postId: {
+    $oid: "0"
+  },
+  _id: {
+    $oid: "0"
+  },
+  accepted: false,
+};
