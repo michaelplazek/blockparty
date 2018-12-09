71b1d5c03e705309d85c99283d5a16dd2e0a69de
Michael Plazek <michael.plazek91@gmail.com>
2018-12-09 13:50:16 -0700

    Clean inputs for log in

--- src/js/screens/Login.js
+++ src/js/screens/Login.js
@@ -7,12 +7,11 @@ import LoginForm from "../components/LoginForm";
 import Grid from "@material-ui/core/Grid/Grid";
 import Typography from "@material-ui/core/Typography/Typography";
 
-import { validateInput } from "../utils/validate";
 import { logInUser } from "../actions/session";
 import mapper from "../utils/connect";
 import Button from "@material-ui/core/Button/Button";
 
-const styles = theme => ({
+const styles = () => ({
   root: {
     background: "white",
     height: "90vh",
@@ -60,9 +59,7 @@ export default compose(
   mapper(propMap, actionMap),
   withHandlers({
     handleLogIn: ({ logInUser, history }) => (username, password) => {
-      if (validateInput(username, password)) {
-        logInUser(username, password, history);
-      }
+      logInUser(username, password, history);
     }
   })
 )(Login);
