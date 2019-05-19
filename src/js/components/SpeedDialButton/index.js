8592289bddf0fa58b8ab194554d01b0dc8b303f8
Michael Plazek <michael.plazek91@gmail.com>
2019-05-18 18:17:19 -0600

    Add speed dial

---
+++ src/js/components/SpeedDialButton/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { compose, withState, withHandlers } from "recompose";
+import SpeedDial from '@material-ui/lab/SpeedDial';
+import SpeedDialIcon from '@material-ui/lab/SpeedDialIcon';
+import CloseIcon from '@material-ui/icons/Close';
+import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
+
+const SpeedDialButton = ({
+  actions,
+  handleClick,
+  handleOpen,
+  handleClose,
+  open,
+}) => (
+  <SpeedDial
+    ariaLabel="Create"
+    icon={<SpeedDialIcon openIcon={<CloseIcon />} />}
+    // onBlur={handleClose}
+    onClick={handleClick}
+    // onClose={handleClose}
+    // onFocus={handleOpen}
+    // onMouseEnter={handleOpen}
+    // onMouseLeave={handleClose}
+    open={open}
+  >
+    /*
+    *  NOTE: this behavior will not work for touch devices
+    *  due issue: https://github.com/mui-org/material-ui/issues/13006
+    * */
+    {actions.map(action => (
+      <SpeedDialAction
+        key={action.name}
+        icon={action.icon}
+        tooltipTitle={action.name}
+        tooltipOpen
+        onClick={() => {
+          action.onClick();
+          handleClick();
+        }}
+      />
+    ))}
+  </SpeedDial>
+);
+
+export default compose(
+  withState('open', 'setOpen', false),
+  withHandlers({
+    handleClick: ({ setOpen, open }) => () => {
+      setOpen(!open);
+    },
+    handleClose: ({ setOpen }) => () => {
+      setOpen(false)
+    },
+    handleOpen: ({ setOpen }) => () => {
+      setOpen(true);
+    },
+  }),
+)(SpeedDialButton);
