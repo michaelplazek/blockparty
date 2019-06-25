5c9f888ecee203ea4bb356bcb6bf5ca68defd93b
Michael Plazek <michael.plazek91@gmail.com>
2019-06-24 18:07:11 -0600

    Update create offer text color

--- src/js/components/Flyout/CreateBidOffer/index.js
+++ src/js/components/Flyout/CreateBidOffer/index.js
@@ -132,7 +132,7 @@ const CreateBidOffer = ({
             background: isDarkMode ? DARK_GREEN : undefined
           }}
         >
-          <Typography>Offer successfully created.</Typography>
+          <Typography color={isDarkMode ? 'textSecondary' : undefined}>Offer successfully created.</Typography>
         </Paper>
       )}
     </Grid>
